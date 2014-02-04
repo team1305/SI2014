f2518ac44fa231564245664d655a581a7f9a98c3
Paul Belanger <paulirino@gmail.com>
2014-02-04 15:52:57 -0500

    Aligned some values in RobotMap

--- src/org/team1305/robot2014/RobotMap.java
+++ src/org/team1305/robot2014/RobotMap.java
@@ -16,44 +16,44 @@ package org.team1305.robot2014;
 public class RobotMap {
     //PWM channels
     //*Chassis
-    public static final int PWM_DRIVE_LEFT_FRONT = 1;
-    public static final int PWM_DRIVE_LEFT_BACK = 2;
+    public static final int PWM_DRIVE_LEFT_FRONT  = 1;
+    public static final int PWM_DRIVE_LEFT_BACK   = 2;
     public static final int PWM_DRIVE_RIGHT_FRONT = 3;
-    public static final int PWM_DRIVE_RIGHT_BACK = 4;
+    public static final int PWM_DRIVE_RIGHT_BACK  = 4;
     //*Catapult
     public static final int PWM_PULLBACK_RIGHT = 5; //Right pullback motor.
-    public static final int PWM_PULLBACK_LEFT = 6; //Left pullback motor.
+    public static final int PWM_PULLBACK_LEFT  = 6; //Left pullback motor.
     //*Claw
-    public static final int PWM_CLAW_LEFT = 7;
+    public static final int PWM_CLAW_LEFT  = 7;
     public static final int PWM_CLAW_RIGHT = 8;
     
     //Relays
     //*Pneumatic
     public static final int REL_COMPRESSOR = 1;
     //*Underglow
-    public static final int REL_UNDERGLOW = 2;
+    public static final int REL_UNDERGLOW  = 2;
     
     //Solenoids
     //*Catapult
     public static final int SOL_LATCH = 1;  //Latches Catapult down for safety.
-    public static final int SOL_GEAR = 2; //Shifts Catapult gear transmission.
+    public static final int SOL_GEAR  = 2; //Shifts Catapult gear transmission.
     
     //Digital I/O
     //*Chassis
     public static final int DIO_ENC_RIGHTWHEEL_P1 = 1; //Encoder on the right drivewheel.
     public static final int DIO_ENC_RIGHTWHEEL_P2 = 2; //Encoder on the right drivewheel.
-    public static final int DIO_ENC_LEFTWHEEL_P1 = 3; //Encoder on the left drivewheel.
-    public static final int DIO_ENC_LEFTWHEEL_P2 = 4; //Encoder on the left drivewheel.
+    public static final int DIO_ENC_LEFTWHEEL_P1  = 3; //Encoder on the left drivewheel.
+    public static final int DIO_ENC_LEFTWHEEL_P2  = 4; //Encoder on the left drivewheel.
     //*Catapult
     public static final int DIO_LIMIT_CAT_BOTTOM = 5;
     //*Pneumatic
-    public static final int DIO_PRESSURE_SENSOR = 6;
+    public static final int DIO_PRESSURE_SENSOR  = 6;
     
     //Analog Channels
     //*Catapult
-    public static final int AN_PULLBACK_POT = 1; //Potentiometer located on the pullback mechanism.
+    public static final int AN_PULLBACK_POT   = 1; //Potentiometer located on the pullback mechanism.
     //*Claw
-    public static final int AN_CLAW_POT_LEFT = 2;
+    public static final int AN_CLAW_POT_LEFT  = 2;
     public static final int AN_CLAW_POT_RIGHT = 4;
     
     
